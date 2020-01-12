f1842084f2e7d0bb439c186906c6adfe87a2bef0
Jonathan Souza <jonathan.wdev@gmail.com>
2020-01-12 18:03:46 -0300

    starting mobile

---
+++ Mobile/src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { Text } from 'react-native';
+// import { Container } from './styles';
+
+export default function src() {
+  return <Text>Hello Las Noches</Text>;
+}
